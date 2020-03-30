d7e71d0bbb361c4daaab5e258745de7fc6225845
Jijun Sui <jijun.sui@aa.com>
2020-03-30 11:27:06 -0500

    gradle

---
+++ src/main/java/com/loyalty/strands/StrandsApplication.java
@@ -0,0 +1,15 @@
+package com.loyalty.strands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrandsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrandsApplication.class, args);
+	}{
+		System.out.println("elp");
+	}
+
+}
---
+++ src/test/java/com/loyalty/strands/StrandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loyalty.strands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
